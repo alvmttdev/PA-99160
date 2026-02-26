ab3f7943c42368663958b7c97da943e9a2f5a4a1
felipe <felipesantosleal.fl@gmail.com>
2026-02-26 20:37:12 -0300

    Novas funções

---
+++ 5. if_else_if.js
@@ -0,0 +1,10 @@
+// Condicionais
+
+const idade = 14;
+
+if (idade > 18) {
+    console.log('A pessoa é maior de idade.');
+}   else if (idade >= 14){    
+    console.log('A pessoa é adolescente.');
+} else {
+        console.log("Infancia"); }
