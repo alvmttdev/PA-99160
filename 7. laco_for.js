dc57416e2945bd646c2d42f22fd7c40c41706c48
felipe <felipesantosleal.fl@gmail.com>
2026-02-26 21:01:23 -0300

    Laço de repetição

---
+++ 7. laco_for.js
@@ -0,0 +1,5 @@
+// LAÇO DE REPETICÇÃO - FOR
+
+for (let i = 5; i > 0; i--) {
+    console.log('O valor de i é: ' + i);
+}
---
+++ 8. while.js
@@ -0,0 +1,7 @@
+// Laço de repetição - while
+
+let i = 1;
+while (i <= 5) {
+    console.log('O valor de i é: ' + i);
+    i++;
+}
