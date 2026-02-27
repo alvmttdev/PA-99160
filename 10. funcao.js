43a223390f316a0caa224bbb7537bb0d66461f1c
felipe <felipesantosleal.fl@gmail.com>
2026-02-26 21:38:26 -0300

    Operadores e funções

---
+++ 10. funcao.js
@@ -0,0 +1,25 @@
+// FUNÇÃO
+
+function somar(a, b) {
+    return a + b;
+}       
+const soma = somar(20, 30);
+console.log(`O resultado da soma é: ${soma}`);
+
+function subtrair(a, b) {
+    return a - b;
+}       
+const subtracao = subtrair(20, 30);
+console.log(`O resultado da subtração é: ${subtracao}`);
+
+function multiplicar(a, b) {
+    return a * b;
+}       
+const multiplicacao = multiplicar(4, 2);
+console.log(`O resultado da multiplicação é: ${multiplicacao}`);
+
+function dividir(a, b) {
+    return a / b;
+}
+const divisao = dividir(40, 2);
+console.log(`O resultado da divisão é: ${divisao}`);
---
+++ 9. operadores.js
@@ -0,0 +1,27 @@
+// OPERADORES LOGICOS
+
+let idade1 = 10;
+let idade2 = "10" 
+console.log("== Comparando conteúdo ==")
+console.log(`A variavel ${idade1} é igual a ${idade2}? `)
+console.log(idade1 == idade2);
+
+console.log("== Comparando conteúdo ==")
+console.log(`A variavel ${idade1} é igual a ${idade2}? `)
+console.log(idade1 != idade2);
+
+console.log("=== Comparando conteúdo e tipo de dado ===")
+console.log(`A variavel ${idade1} é igual a ${idade2}? `)
+console.log(idade1 === idade2);
+
+console.log("=== Comparando conteúdo e tipo de dado ===")
+console.log(`A variavel ${idade1} é igual a ${idade2}? `)
+console.log(idade1 !== idade2);
+
+console.log("=== AND ===")
+console.log("1 < 2 && 1 < 3");
+console.log(1 < 2 && 1 < 3);
+
+console.log("=== OR ===")
+console.log("10 > 2 || 1 < 3");
+console.log(10 > 2 || 1 < 3);
